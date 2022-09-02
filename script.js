bcbac35f640fb2603945a41ca68d29f09f035561
JackPadalino <pada0867@gmail.com>
2022-09-02 01:01:13 -0400

    updated painting with mousedown and mouseover

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
+const table=document.getElementById('table');
+const select=document.getElementById('select');
+const addRowButton=document.getElementById('add-row');
+const resetButton=document.getElementById('reset');
+const bucketButton=document.getElementById('bucket');
+const deleteButton=document.getElementById('delete-board');
+const startButton=document.getElementById('start-drawing');
+const startMenu=document.getElementById('start-menu');
+const mainBoard=document.getElementById('main-board');
+
+let chosenColor='';
+
+mainBoard.style.display='none';
+
+const makeRows=()=>{
+    for(let a=0;a<10;a++){
+        const newRow = document.createElement('tr')
+        for(let i=0;i<50;i++){
+            const newPixel=document.createElement('td');
+            newPixel.className='light-gray';
+            newRow.appendChild(newPixel);
+        };
+        table.appendChild(newRow);
+    };
+};
+
+const colorize=(event)=>{
+    const target=event.target;
+    if(target.tagName==='TD'){
+        target.className=chosenColor;
+    };
+    table.addEventListener('mouseover',colorize);
+};
+
+// add ten rows of 100 tiles
+addRowButton.addEventListener('click',makeRows);
+
+// selecting a color from the menu
+select.addEventListener('change',(event)=>{
+    chosenColor=event.target.value;
+});
+
+// click and drag to paint
+table.addEventListener("mousedown", colorize);
+table.addEventListener("mouseup", () =>
+  table.removeEventListener("mouseover", colorize)
+  );
+
+// fill all non-colored tiles with selected color
+bucketButton.addEventListener('click',function(){
+    const tiles=[...document.getElementsByTagName('td')];
+    tiles.forEach(tile=>{
+        if(tile.className==='light-gray'){
+            tile.className=chosenColor;
+        };
+    });
+}); 
+
+// reset all tiles
+resetButton.addEventListener('click',function(){
+    const tiles=[...document.getElementsByTagName('td')];
+    tiles.forEach(tile=>{
+        tile.className='light-gray';
+    });
+});
+
+
+// delete board
+deleteButton.addEventListener('click',function(){
+    while(table.firstChild){
+        table.removeChild(table.firstChild);
+    };
+});
+
+// show/hide divs
+startButton.addEventListener('click',function(){
+    startMenu.style.display='none';
+    mainBoard.style.display='block';
+});
+
+
+
+
+
 //~~~~~program with JQuery~~~~~//
 /*
 const table=$('#table');
@@ -83,94 +167,3 @@ startButton.click(function(){
     mainBoard.show();
 });
 */
-
-
-
-
-
-
-
-
-
-
-
-//~~~~~program without JQuery~~~~~//
-const table=document.getElementById('table');
-const select=document.getElementById('select');
-const addRowButton=document.getElementById('add-row');
-const resetButton=document.getElementById('reset');
-const bucketButton=document.getElementById('bucket');
-const deleteButton=document.getElementById('delete-board');
-const startButton=document.getElementById('start-drawing');
-const startMenu=$('#start-menu');
-const mainBoard=$('#main-board');
-
-let chosenColor='';
-let mouseIsDown=false;
-
-mainBoard.hide();
-
-const makeRows=()=>{
-    for(let a=0;a<10;a++){
-        const newRow = document.createElement('tr')
-        for(let i=0;i<50;i++){
-            const newPixel=document.createElement('td');
-            newPixel.className='light-gray';
-            newRow.appendChild(newPixel);
-        };
-        table.appendChild(newRow);
-    };
-};
-
-const colorize=(event)=>{
-    const target=event.target;
-    console.log(event);
-    if(target.tagName==='TD' && mouseIsDown){
-        target.className=chosenColor;
-    };
-};
-
-// add ten rows of 100 tiles
-addRowButton.addEventListener('click',makeRows);
-
-// selecting a color from the menu
-select.addEventListener('change',(event)=>{
-    chosenColor=event.target.value;
-});
-
-// click and drag to paint
-table.addEventListener('mousedown',()=>{mouseIsDown=true});
-table.addEventListener('mouseup',()=>{mouseIsDown=false});
-table.addEventListener('mouseover',colorize);
-
-// fill all non-colored tiles with selected color
-bucketButton.addEventListener('click',function(){
-    const tiles=[...document.getElementsByTagName('td')];
-    tiles.forEach(tile=>{
-        if(tile.className==='light-gray'){
-            tile.className=chosenColor;
-        };
-    });
-}); 
-
-// reset all tiles
-resetButton.addEventListener('click',function(){
-    const tiles=[...document.getElementsByTagName('td')];
-    tiles.forEach(tile=>{
-        tile.className='light-gray';
-    });
-});
-
-
-// delete board
-deleteButton.addEventListener('click',function(){
-    while(table.firstChild){
-        table.removeChild(table.firstChild);
-    };
-});
-
-// show/hide divs
-startButton.addEventListener('click',function(){
-    startMenu.hide();
-    mainBoard.show();
-});
